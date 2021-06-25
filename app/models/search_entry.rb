4fa8fa6862eaaaeb9967d1f6f3ff826ff0d905df
Steve Polito <stevepolito@hey.com>
2021-06-25 07:29:40 -0400

    Add uniqueness scope.

--- app/models/search_entry.rb
+++ app/models/search_entry.rb
@@ -1,3 +1,5 @@
 class SearchEntry < ApplicationRecord
   delegated_type :searchable, types: %w[ Post User ]
+
+  validates :searchable_type, uniqueness: { scope: :searchable_id }
 end
--- test/models/search_entry_test.rb
+++ test/models/search_entry_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class SearchEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @search_entry = SearchEntry.new(searchable: posts(:one))
+  end
+
+  test "should be valid" do
+    assert @search_entry.valid?
+  end
+
+  test "uniqueness scope" do
+    @search_entry.save
+    @duplicate_search_entry = SearchEntry.new(searchable: posts(:one))
+    assert_not @duplicate_search_entry.valid?
+  end
 end
