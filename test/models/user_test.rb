343eda5ae4aaa8c50f72be9e3136f96b147499b7
Steve Polito <stevepolito@hey.com>
2021-06-26 12:11:14 -0400

    WIP: Write failing tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new(name: "Name", biography: "Biography")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should create search_entry on create" do
+    assert_difference("SearchEntry.count", 1) do
+      @user.save!
+    end
+    assert_equal @user.search_entry.title, @user.name
+    assert_equal @user.search_entry.body, @user.biography
+  end
+
+  test "should update search_entry on update" do
+    updated_name        = "Updated Title"
+    updated_biography   = "Updated Biography"
+    
+    @user = users(:one)
+
+    assert_no_difference("SearchEntry.count") do
+      @user.update(title: updated_name, body: updated_biography)
+    end
+
+    assert_equal updated_name, @user.search_entry.title
+    assert_equal updated_biography, @user.search_entry.body
+  end  
+
+  test "should destroy search_entry on destroy" do
+    @user = users(:one)
+    assert_difference("SearchEntry.count", -1) do
+      @user.destroy
+    end
+  end
 end
