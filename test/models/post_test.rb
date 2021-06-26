a878ab5d82f74bd8410577751524b593932979d8
Steve Polito <stevepolito@hey.com>
2021-06-26 11:50:06 -0400

    WIP: Failing tests.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = Post.new(title: "Title", body: "Body")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "should create search_entry on create" do
+    assert_difference("SearchEntry.count", 1) do
+      @post.save!
+    end
+    assert_equal @post.search_entry.title, @post.title
+    assert_equal @post.search_entry.body, @post.body
+  end
+
+  test "should update search_entry on update" do
+    updated_title = "Updated Title"
+    updated_body  = "Updated Body"
+    
+    @post = posts(:one)
+
+    assert_no_difference("SearchEntry.count") do
+      @post.update(title: updated_title, body: updated_body)
+    end
+
+    assert_equal updated_title, @post.search_entry.title
+    assert_equal updated_body, @post.search_entry.body
+  end  
+
+  test "should destroy search_entry on destroy" do
+    @post = posts(:one)
+    assert_difference("SearchEntry.count", -1) do
+      @post.destroy
+    end
+  end
 end
