cd33e339f957665dbe5467659ee952a4dfd4a952
Steve Polito <stevepolitodesign@users.noreply.github.com>
2021-06-26 22:22:03 -0400

    Update search_entries_controller.rb

--- app/controllers/search_entries_controller.rb
+++ app/controllers/search_entries_controller.rb
@@ -1,5 +1,5 @@
 class SearchEntriesController < ApplicationController
   def index
-    @search_entries = SearchEntry.where("title LIKE ? OR body LIKE? ", "%#{params[:query]}%", "%#{params[:query]}%") if params[:query]
+    @search_entries = SearchEntry.where("title LIKE ? OR body LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%") if params[:query]
   end
 end
